4a63968c8784eec56080da3cb55b152a91e11a30
tcbalch <tcbalch@gmail.com>
2024-10-10 02:40:36 -0400

    first try

--- balch_demo4/app/__init__.py
+++ balch_demo4/app/__init__.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import g
+from .app_factory import create_app
+from .db_connect import get_db, close_db
 
-app = Flask(__name__)
+app = create_app()
 
-from app import routes  # Ensure that routes are imported after initializing the app
+@app.before_request
+def before_request():
+    g.db = get_db()
+
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    close_db(exception)
+
+
 
